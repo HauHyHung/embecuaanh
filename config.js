fc41ee2431d55d6265ea031311fa776d7e170382
Hau <163385034+HauHyHung@users.noreply.github.com>
2024-05-14 09:19:48 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
+const nameGirl = 'Bảo Trân';
 const giftUrl = 'http://nodemy.vn';
-const eventName = 'Chúc công chúa Bảo Trân ngày mới vui vẻ';
+const eventName = 'Bủi sáng vui vẻ công chúa';
 const titleCard = 'Gửi công chúa xinh đẹp';
-const contentCard = 'Công chúa đi xe về cẩn thẩn, hong mệt mỏi nhaaa. Về tới vương quốc Long Xuyên thì có binh lính bộ tống rồi';
+const contentCard = 'Công chúa đi đường xa cẩn thẩn,  khỏe re luôn. Về tới vương quốc Long Xuyên thì có binh lính bộ tống rồi';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
